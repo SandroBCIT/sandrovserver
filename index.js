69fe41b68a0a1b70ae3ed49366c84bdb721fc343
unknown <alessandro.grunwald@gmail.com>
2018-02-06 17:12:45 -0800

    a

---
+++ index.js
@@ -0,0 +1,37 @@
+const server = require("http").Server(); 
+const port = 10001;
+
+var io = require("socket.io")(server);
+
+var names = [];
+var msgs = [];
+
+io.on("connection", function(socket){
+    console.log("user has connected");
+    
+    socket.on("uname", function(data){
+        console.log("username sent = " + data); 
+        names.push(data);
+        
+        io.emit("names", names);
+    });
+    
+    socket.on("sendmsg", function(data){
+        console.log("the msg = " + data); 
+        msgs.push(data);
+        
+        io.emit("msgs", msgs);
+    });
+    
+    socket.on("disconnect", function(){
+        console.log("User has disconnected");
+    })
+});
+
+server.listen(port, (err)=>{
+    if(err){
+        console.log("error: " + err);
+        return false;
+    }
+    console.log("Socket port is running");
+})
